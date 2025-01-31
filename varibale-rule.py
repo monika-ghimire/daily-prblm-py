28e39885961f7355f87ae546dfceb93e1d7b3da7
Monika ghimire <monika.ghimirey@boosoft.com.au>
2025-01-31 21:10:05 +0545

    rules for variables

---
+++ varibale-rule.py
@@ -0,0 +1,12 @@
+# allowed
+"""alpha a to z 
+number 0 - 9
+star with only alpha not number
+can use _ underscrore"""
+
+# here list what not allwed while making variable 
+
+"""
+@a=1 # not allowed
+not white space or number in start
+"""
